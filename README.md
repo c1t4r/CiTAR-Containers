60cf23c9931ab3f78790c26429af8574d2a4e9d1
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2017-06-13 16:03:11 +0200

    updated build

--- README.md
+++ README.md
@@ -21,3 +21,5 @@ They can be pulled using docker or singularity as follows:
 * updated build: 2017-06-13-14-42
 
 * updated build: 2017-06-13-15-10
+
+* updated build: 2017-06-13-16-03
--- imports.sh
+++ imports.sh
@@ -1,3 +1,6 @@
 mkdir -p ./imports//usr/local/ && rsync -raphvz /usr/local/ ./imports//usr/local/
 mkdir -p ./imports// && rsync -raphvz /nfs/home1/ul/ul_kiz/ul_l_nsn25/Plurality/rootfs/* ./imports//
 mkdir -p ./imports// && rsync -raphvz /nfs/home1/ul/ul_kiz/ul_l_nsn25/Plurality/rootfs/.singularity.d ./imports//
+mkdir -p ./imports//usr/local/ && rsync -raphvz /usr/local/ ./imports//usr/local/
+mkdir -p ./imports// && rsync -raphvz /nfs/home1/ul/ul_kiz/ul_l_nsn25/Plurality/rootfs/* ./imports//
+mkdir -p ./imports// && rsync -raphvz /nfs/home1/ul/ul_kiz/ul_l_nsn25/Plurality/rootfs/.singularity.d ./imports//
